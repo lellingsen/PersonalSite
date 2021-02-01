9515a4fbe013b0528eb764e934a90ebde40631a3
Lars Ellingsen <lellingsen17@gmail.com>
2021-01-31 18:31:37 -0800

    Adding open sans

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import "fontsource-open-sans"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
