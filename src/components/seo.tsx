a4800f5982f44115b30494c1b1206388d721ae25
Lars Ellingsen <lellingsen17@gmail.com>
2021-02-02 18:28:34 -0800

    Adding back in Twitter headers

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -61,6 +61,18 @@ const SEO = ({ description, lang, meta, title }: Props) => {
           property: `og:type`,
           content: `website`,
         },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
       ].concat(meta || [])}
     />
   )
