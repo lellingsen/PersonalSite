8109b3e59c75c40468d4df093f545b9e2432772f
Lars Ellingsen <lellingsen17@gmail.com>
2021-02-04 14:15:56 -0800

    bio component totall on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
@@ -5,6 +7,15 @@ module.exports = {
     fontFamily: {
       sans: ["Open Sans", "sans-serif"],
       serif: ["Merriweather", "serif"],
+    },
+    colors: {
+      gray: colors.coolGray,
+      cyan: colors.cyan,
+      violet: colors.violet,
+      teal: colors.teal,
+    },
+    minWidth: {
+      50: "50px",
     },
     extend: {},
   },
