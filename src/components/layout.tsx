85bc1e5d4bff5578ee4ab0b33f0b5b6ee8188773
Lars Ellingsen <lellingsen17@gmail.com>
2021-02-26 20:46:19 -0800

    Starting to get nav working, not satisfied with styling yet

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -16,10 +16,31 @@ const Layout = ({ location, title, children }: Props) => {
       data-is-root-path={isRootPath}
       className="flex flex-col h-screen justify-between text-gray-800"
     >
-      <header className="mb-8 p-4 w-full bg-teal-900 text-gray-100">
-        <Link to="/">
-          <h1 className="text-5xl lg:w-1/2 mx-auto">{title}</h1>
-        </Link>
+      <header className="mb-8 pt-4 px-4 w-full bg-teal-900 text-gray-100">
+        <div className="flex flex-row flex-wrap justify-between lg:w-1/2 mx-auto pb-4">
+          <h1 className="text-5xl">
+            <Link to="/">{title}</Link>
+          </h1>
+          <label htmlFor="menu-toggle" className="text-5xl cursor-pointer">
+            Menu
+          </label>
+        </div>
+        <div className="flex flex-row mx-auto">
+          <input type="checkbox" id="menu-toggle" className="hidden" />
+          <nav role="navigation" className="nav w-full mt-4">
+            <ul className="text-center">
+              <li className="block p-4 text-2xl border-gray-400 border-t-2">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="block p-4 text-2xl border-gray-400 border-t-2">
+                <Link to="/about">About Lars</Link>
+              </li>
+              <li className="block p-4 text-2xl border-gray-400 border-t-2">
+                <Link to="/tags">Tags</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
       <main className="w-full lg:w-1/2 mx-auto p-4 mb-auto">{children}</main>
       <footer className="w-full p-4 text-center bg-teal-900 text-gray-100">
